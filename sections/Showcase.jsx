a8f9ef4a8d52dc96c07d8c0dc4ee7cb5c27a2fdf
mahir-m01 <mahirabd.official@gmail.com>
2025-04-27 20:06:55 +0530

    fixes

--- sections/Showcase.jsx
+++ sections/Showcase.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import HeroExperience from "@/components/HeroModel/HeroExperience";
-import WaterAnimation from "@/sections/WaterAnimation";
 
 const Showcase = () => {
     return (
-        <setion className="container mx-auto flex flex-col items-center pb-12 px-3">
+        <section className="container mx-auto flex flex-col items-center pb-12 px-3">
             <figure className="w-full h-[50vh] md:h-[60vh] lg:h-[70vh] mx-auto">
                 <HeroExperience />
             </figure>
 
-        </setion>
+        </section>
     )
 }
 export default Showcase
