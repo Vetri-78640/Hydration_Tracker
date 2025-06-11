45233bf125cb4ec3a3e1306476425271111978b3
mahir-m01 <mahirabd.official@gmail.com>
2025-06-11 16:18:28 +0530

    metadata changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,8 +4,8 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export const metadata = {
-    title: "Hydration Tracker",
-    description: "Track your daily water intake",
+    title: "Hydraze",
+    description: "Hydraze is a minimalist open-source web app that helps you monitor your daily water intake, with weather-based suggestions and hydration facts to keep you healthy and motivated.",
 };
 
 export default function RootLayout({ children }) {
